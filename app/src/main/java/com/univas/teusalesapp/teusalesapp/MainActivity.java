de903f657310d445a86f9b909640abdc72a0059a
unknown <djbzombie@hotmail.com>
2018-07-24 22:40:06 -0300

    navigation drawer header

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/MainActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/MainActivity.java
@@ -1,13 +1,73 @@
 package com.univas.teusalesapp.teusalesapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //inicializar layouts: nav, drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+
+    }
+
+    //acesso as opções do navigation
+    private void UserMenuSelector(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_profile:
+                Toast.makeText(this,"Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_home:
+                Toast.makeText(this,"Home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_friends:
+                Toast.makeText(this,"Friend List", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_find_friends:
+                Toast.makeText(this,"Find Friends", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_messages:
+                Toast.makeText(this,"Messages", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_Logout:
+                Toast.makeText(this,"Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
