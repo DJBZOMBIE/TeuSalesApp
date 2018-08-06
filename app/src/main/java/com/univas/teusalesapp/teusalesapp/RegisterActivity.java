3bf020d53fc98e3b49b2d7230e71643939ad73e2
unknown <djbzombie@hotmail.com>
2018-08-05 22:38:36 -0300

    Register user Firebase Authentication

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/RegisterActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/RegisterActivity.java
@@ -1,23 +1,101 @@
 package com.univas.teusalesapp.teusalesapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText UserEmail, UserPassword, UserConfirmPassword;
     private Button CreateAccountButton;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         UserEmail = (EditText) findViewById(R.id.register_email);
         UserPassword = (EditText) findViewById(R.id.register_password);
         UserConfirmPassword = (EditText) findViewById(R.id.register_confirm_password);
         CreateAccountButton = (Button) findViewById(R.id.register_create_account);
+        loadingBar = new ProgressDialog(this);
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewAccount();
+            }
+        });
+    }
+
+    //criar conta
+    private void CreateNewAccount(){
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfirmPassword.getText().toString();
+
+        //validações
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Por favor escreva o seu E-mail...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Por favor escreva a sua senha...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(confirmPassword)){
+            Toast.makeText(this, "Por favor confirme a sua senha...", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals(confirmPassword)){
+            Toast.makeText(this, "Sua senha esta diferente...", Toast.LENGTH_SHORT).show();
+        }else{
+            loadingBar.setTitle("Criando Nova Conta");
+            loadingBar.setMessage("Por favor, aguarde enquanto criamos a sua conta nova conta...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                SendUserToSetupActivity();
+                                
+                                Toast.makeText(RegisterActivity.this, "Sua conta foi criada com sucesso!", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }else{
+                                String message = task.getException().getMessage();
+                                Toast.makeText(RegisterActivity.this, "Erro: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    //enviar o usuário para activity de configuração
+    private void SendUserToSetupActivity() {
+        Intent setupIntent = new Intent(RegisterActivity.this, SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
     }
 }
