8d654c7b2c9de9bdde3fcad86371736679e74c7d
unknown <djbzombie@hotmail.com>
2018-09-09 01:00:56 -0300

    App Bar Layout - Toolbar Back Button

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/PostActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/PostActivity.java
@@ -1,13 +1,43 @@
 package com.univas.teusalesapp.teusalesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar); //importar: android.support.v7.widget.Toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+    }
+
+
+    //botão de fazer voltar para a mainActivity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            SendUserToMainActivity();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
