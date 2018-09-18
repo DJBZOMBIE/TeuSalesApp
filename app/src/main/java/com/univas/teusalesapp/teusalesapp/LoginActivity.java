b62ad96b72da11bd7ff2a7efb2ffa35bd33bfab9
unknown <djbzombie@hotmail.com>
2018-09-18 03:07:07 -0300

    Firebase Google sign in | authenticate user

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/LoginActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/LoginActivity.java
@@ -2,30 +2,46 @@ package com.univas.teusalesapp.teusalesapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button LoginButton;
+    private ImageView googleSignInButton;
     private EditText UserEmail, UserPassword;
     private TextView NeedNewAccountLink;
     private ProgressDialog loadingBar;
 
     private FirebaseAuth mAuth;
+
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleSignInClient;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
         LoginButton = (Button) findViewById(R.id.login_button);
-
+        googleSignInButton = (ImageView) findViewById(R.id.google_signin_button);
         loadingBar = new ProgressDialog(this);
 
         NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
@@ -55,8 +71,92 @@ public class LoginActivity extends AppCompatActivity {
                 AllowingUserToLogin();
             }
         });
-        
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        //set google API
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "A conexão com a conta do Google falhou...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        //ao clicar no botão google sign
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
     }
+
+    //======= inicio login com conta do google =======
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+
+            loadingBar.setTitle("Google Login");
+            loadingBar.setMessage("Por favor, aguarde enquanto fazemos login usando sua conta do Google...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data); //data igual google intent
+
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);//passa a conta para ser autenticada
+                Toast.makeText(this, "Por favor, aguarde enquanto estamos verificando os resultados da sua conta...", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Não foi possível verificar os dados da sua conta do Google.", Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            SendUserToMainActivity();
+                            loadingBar.dismiss();
+
+                        } else {
+                            // If Google sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            String message = task.getException().toString();
+                            SendUserToLoginActivity();
+                            Toast.makeText(LoginActivity.this, "Não autenticado: " + message , Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+    }
+
+
+    //======= fim login com conta do google =======
 
     //se o usuário já esta logado, então envia-o diretamente para o main activity(evita de ir toda hora para setup activity sempre que fizer login)
     @Override
@@ -81,8 +181,8 @@ public class LoginActivity extends AppCompatActivity {
         }else{
             loadingBar.setTitle("Login");
             loadingBar.setMessage("Por favor, aguarde enquanto fazemos login em sua conta...");
-            loadingBar.show();
             loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
 
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -105,6 +205,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private void SendUserToMainActivity() {
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, LoginActivity.class);
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(mainIntent);
         finish();
