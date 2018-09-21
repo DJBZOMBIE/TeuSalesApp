6cc78a98a43a26bcacf2e65a7247c237114cbfc6
unknown <djbzombie@hotmail.com>
2018-09-21 01:41:05 -0300

    click post activity

---
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/ClickPostActivity.java
@@ -0,0 +1,26 @@
+package com.univas.teusalesapp.teusalesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ClickPostActivity extends AppCompatActivity {
+
+    private ImageView PostImage;
+    private TextView PostDescription;
+    private Button DeletePostButton, EditPostButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_post);
+
+        PostImage = (ImageView) findViewById(R.id.post_image);
+        PostDescription = (TextView) findViewById(R.id.post_description);
+        DeletePostButton = (Button) findViewById(R.id.delete_post_button);
+        EditPostButton = (Button) findViewById(R.id.edit_post_button);
+
+    }
+}
