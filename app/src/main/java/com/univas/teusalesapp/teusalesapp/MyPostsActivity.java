7db475554fac33b9ca056b484b15076e7383e369
unknown <djbzombie@hotmail.com>
2018-10-21 16:57:24 -0300

    add toolbar in activity_my_posts layout

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/MyPostsActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/MyPostsActivity.java
@@ -2,12 +2,21 @@ package com.univas.teusalesapp.teusalesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MyPostsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
+
+        mToolbar = (Toolbar) findViewById(R.id.my_posts_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Minhas Postagens");
     }
 }
