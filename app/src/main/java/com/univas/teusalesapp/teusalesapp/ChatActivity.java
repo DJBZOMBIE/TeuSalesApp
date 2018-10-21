0932324bc8c689779f0a3856edc95ef89fd957db
unknown <djbzombie@hotmail.com>
2018-10-21 11:39:39 -0300

    Send Message

--- app/src/main/java/com/univas/teusalesapp/teusalesapp/ChatActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/ChatActivity.java
@@ -1,23 +1,34 @@
 package com.univas.teusalesapp.teusalesapp;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,29 +39,88 @@ public class ChatActivity extends AppCompatActivity {
     private EditText userMessageInput;
     private RecyclerView userMessagesList;
 
-    private String messageReceiverID, messageReceiberName;
+    private String messageReceiverID, messageReceiverName, messageSenderID, saveCurrentDate, saveCurrentTime;
 
     private TextView receiverName;
     private CircleImageView receiverProfileImage;
     private DatabaseReference RootRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        mAuth = FirebaseAuth.getInstance();
+        messageSenderID = mAuth.getCurrentUser().getUid();
+
         RootRef = FirebaseDatabase.getInstance().getReference();
 
         messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
-        messageReceiberName= getIntent().getExtras().get("username").toString();
+        messageReceiverName= getIntent().getExtras().get("username").toString();
 
         IntializeFields();
         DisplayReceiverInfo();
+
+        SendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendMessage();
+            }
+        });
+    }
+
+    //enviar mensagem
+    private void SendMessage() {
+        String messageText = userMessageInput.getText().toString();
+
+        if(TextUtils.isEmpty(messageText)){
+            Toast.makeText(this, "Por favor digite uma mensagem primeiro...", Toast.LENGTH_SHORT).show();
+        }else{
+            String message_sender_ref = "Messages/" + messageSenderID + "/" + messageReceiverID; //referencia o id (do usuario) que enviou mensagem (montagem da arvore de mensagens enviadas no database)
+            String message_receiver_ref = "Messages/" + messageReceiverID + "/" + messageSenderID; //refencia o id (do usuario) que recebeu a mensagem
+            DatabaseReference user_message_key = RootRef.child("Messages").child(messageSenderID).child(messageReceiverID).push();//push cria um id unico para as mensagens
+            String message_push_id = user_message_key.getKey();
+
+            Calendar calFordDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy"); //data padrão
+            saveCurrentDate = currentDate.format(calFordDate.getTime()); //pega data padrao e salva na var saveCurrentDate
+            Calendar calFordTime = Calendar.getInstance();
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss"); //hora/tempo padrão
+            saveCurrentTime = currentTime.format(calFordTime.getTime());
+
+            //salvar mensagem no bd
+            Map messageTextBody = new HashMap();
+                messageTextBody.put("message", messageText);
+                messageTextBody.put("time", saveCurrentTime);
+                messageTextBody.put("date", saveCurrentDate);
+                messageTextBody.put("type", "text");
+                messageTextBody.put("from", messageSenderID);
+
+            //salvar os id's dos users que enviaram/receberam mensagens
+           Map messageBodyDetails = new HashMap();
+               messageBodyDetails.put(message_sender_ref + "/" + message_push_id, messageTextBody);
+               messageBodyDetails.put(message_receiver_ref + "/" + message_push_id, messageTextBody);
+
+           //salva a arvore de mensagens no BD
+           RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+               @Override
+               public void onComplete(@NonNull Task task) {
+                   if(task.isSuccessful()){
+                       Toast.makeText(ChatActivity.this, "Mensagem enviada com sucesso.", Toast.LENGTH_SHORT).show();
+                   }else{
+                       String message = task.getException().getMessage();
+                       Toast.makeText(ChatActivity.this, "Erro: " + message, Toast.LENGTH_SHORT).show();
+                   }
+                   userMessageInput.setText(""); //limpa a variavel de texto
+               }
+           });
+        }
     }
 
     //mostra o nome e foto do usuario na barra de titulo do chat
     private void DisplayReceiverInfo() {
-       receiverName.setText(messageReceiberName);
+       receiverName.setText(messageReceiverName);
 
        RootRef.child("Users").child(messageReceiverID).addValueEventListener(new ValueEventListener() {
            @Override
--- app/src/main/java/com/univas/teusalesapp/teusalesapp/FriendsActivity.java
+++ app/src/main/java/com/univas/teusalesapp/teusalesapp/FriendsActivity.java
@@ -95,7 +95,7 @@ public class FriendsActivity extends AppCompatActivity {
                                            if(i == 1){
                                                Intent chatintent = new Intent(FriendsActivity.this, ChatActivity.class);
                                                chatintent.putExtra("visit_user_id", usersIDs);
-                                               chatintent.putExtra("userName", userName);
+                                               chatintent.putExtra("username", userName);
                                                startActivity(chatintent);
                                            }
                                        }
